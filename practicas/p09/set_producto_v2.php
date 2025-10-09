37e2d8479df12814ae3e118c4bece71f9ecacbb1
m4rvick <brunomarck4@gmail.com>
2025-10-08 19:25:53 -0600

    practica 9: query en set_producto_v2 modificada (atributo eliminado), 2 productos agregados

--- practicas/p09/set_producto_v2.php
+++ practicas/p09/set_producto_v2.php
@@ -38,7 +38,7 @@ if ($result->num_rows > 0) {
 
 
 /** Crear una tabla que no devuelve un conjunto de resultados */
-$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
 if ( $link->query($sql) ) 
 {
     echo 'Producto insertado con ID: '.$link->insert_id;
