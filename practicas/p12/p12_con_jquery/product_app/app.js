46d0e63a2bd2c79bdfc1f24274cb5679e58f6a5f
m4rvick <brunomarck4@gmail.com>
2025-10-24 19:56:07 -0600

    practica 12: copia de la carpeta con jquery

--- practicas/p12/p12_con_jquery/product_app/app.js
+++ practicas/p12/p12_con_jquery/product_app/app.js
@@ -6,14 +6,14 @@ var baseJSON = {
     "marca": "NA",
     "detalles": "NA",
     "imagen": "img/default.png"
-  };
+};
 
 function init() {
     /**
      * Convierte el JSON a string para poder mostrarlo
      * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
      */
-    var JsonString = JSON.stringify(baseJSON,null,2);
+    var JsonString = JSON.stringify(baseJSON, null, 2);
     document.getElementById("description").value = JsonString;
 
     // SE LISTAN TODOS LOS PRODUCTOS
@@ -30,12 +30,12 @@ function listarProductos() {
         // SE VERIFICA SI LA RESPUESTA ESTÁ LISTA Y FUE SATISFACTORIA
         if (client.readyState == 4 && client.status == 200) {
             //console.log('[CLIENTE]\n'+client.responseText);
-            
+
             // SE OBTIENE EL OBJETO DE DATOS A PARTIR DE UN STRING JSON
             let productos = JSON.parse(client.responseText);    // similar a eval('('+client.responseText+')');
-            
+
             // SE VERIFICA SI EL OBJETO JSON TIENE DATOS
-            if(Object.keys(productos).length > 0) {
+            if (Object.keys(productos).length > 0) {
                 // SE CREA UNA PLANTILLA PARA CREAR LAS FILAS A INSERTAR EN EL DOCUMENTO HTML
                 let template = '';
 
@@ -45,12 +45,12 @@ function listarProductos() {
 
                     // SE CREA UNA LISTA HTML CON LA DESCRIPCIÓN DEL PRODUCTO
                     let descripcion = '';
-                    descripcion += '<li>precio: '+producto.precio+'</li>';
-                    descripcion += '<li>unidades: '+producto.unidades+'</li>';
-                    descripcion += '<li>modelo: '+producto.modelo+'</li>';
-                    descripcion += '<li>marca: '+producto.marca+'</li>';
-                    descripcion += '<li>detalles: '+producto.detalles+'</li>';
-                
+                    descripcion += '<li>precio: ' + producto.precio + '</li>';
+                    descripcion += '<li>unidades: ' + producto.unidades + '</li>';
+                    descripcion += '<li>modelo: ' + producto.modelo + '</li>';
+                    descripcion += '<li>marca: ' + producto.marca + '</li>';
+                    descripcion += '<li>detalles: ' + producto.detalles + '</li>';
+
                     template += `
                         <tr productId="${producto.id}">
                             <td>${producto.id}</td>
@@ -86,18 +86,18 @@ function buscarProducto(e) {
 
     // SE CREA EL OBJETO DE CONEXIÓN ASÍNCRONA AL SERVIDOR
     var client = getXMLHttpRequest();
-    client.open('GET', './backend/product-search.php?search='+search, true);
+    client.open('GET', './backend/product-search.php?search=' + search, true);
     client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     client.onreadystatechange = function () {
         // SE VERIFICA SI LA RESPUESTA ESTÁ LISTA Y FUE SATISFACTORIA
         if (client.readyState == 4 && client.status == 200) {
             //console.log('[CLIENTE]\n'+client.responseText);
-            
+
             // SE OBTIENE EL OBJETO DE DATOS A PARTIR DE UN STRING JSON
             let productos = JSON.parse(client.responseText);    // similar a eval('('+client.responseText+')');
-            
+
             // SE VERIFICA SI EL OBJETO JSON TIENE DATOS
-            if(Object.keys(productos).length > 0) {
+            if (Object.keys(productos).length > 0) {
                 // SE CREA UNA PLANTILLA PARA CREAR LAS FILAS A INSERTAR EN EL DOCUMENTO HTML
                 let template = '';
                 let template_bar = '';
@@ -108,12 +108,12 @@ function buscarProducto(e) {
 
                     // SE CREA UNA LISTA HTML CON LA DESCRIPCIÓN DEL PRODUCTO
                     let descripcion = '';
-                    descripcion += '<li>precio: '+producto.precio+'</li>';
-                    descripcion += '<li>unidades: '+producto.unidades+'</li>';
-                    descripcion += '<li>modelo: '+producto.modelo+'</li>';
-                    descripcion += '<li>marca: '+producto.marca+'</li>';
-                    descripcion += '<li>detalles: '+producto.detalles+'</li>';
-                
+                    descripcion += '<li>precio: ' + producto.precio + '</li>';
+                    descripcion += '<li>unidades: ' + producto.unidades + '</li>';
+                    descripcion += '<li>modelo: ' + producto.modelo + '</li>';
+                    descripcion += '<li>marca: ' + producto.marca + '</li>';
+                    descripcion += '<li>detalles: ' + producto.detalles + '</li>';
+
                     template += `
                         <tr productId="${producto.id}">
                             <td>${producto.id}</td>
@@ -134,7 +134,7 @@ function buscarProducto(e) {
                 // SE HACE VISIBLE LA BARRA DE ESTADO
                 document.getElementById("product-result").className = "card my-4 d-block";
                 // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
-                document.getElementById("container").innerHTML = template_bar;  
+                document.getElementById("container").innerHTML = template_bar;
                 // SE INSERTA LA PLANTILLA EN EL ELEMENTO CON ID "productos"
                 document.getElementById("products").innerHTML = template;
             }
@@ -154,14 +154,14 @@ function agregarProducto(e) {
     // SE AGREGA AL JSON EL NOMBRE DEL PRODUCTO
     finalJSON['nombre'] = document.getElementById('name').value;
     // SE OBTIENE EL STRING DEL JSON FINAL
-    productoJsonString = JSON.stringify(finalJSON,null,2);
+    productoJsonString = JSON.stringify(finalJSON, null, 2);
 
-/**
- * AQUÍ DEBES AGREGAR LAS VALIDACIONES DE LOS DATOS EN EL JSON
- * ...
- * 
- * --> EN CASO DE NO HABER ERRORES, SE ENVIAR EL PRODUCTO A AGREGAR
- */
+    /**
+     * AQUÍ DEBES AGREGAR LAS VALIDACIONES DE LOS DATOS EN EL JSON
+     * ...
+     * 
+     * --> EN CASO DE NO HABER ERRORES, SE ENVIAR EL PRODUCTO A AGREGAR
+     */
 
     // SE CREA EL OBJETO DE CONEXIÓN ASÍNCRONA AL SERVIDOR
     var client = getXMLHttpRequest();
@@ -194,13 +194,13 @@ function agregarProducto(e) {
 
 // FUNCIÓN CALLBACK DE BOTÓN "Eliminar"
 function eliminarProducto() {
-    if( confirm("De verdad deseas eliinar el Producto") ) {
+    if (confirm("De verdad deseas eliinar el Producto")) {
         var id = event.target.parentElement.parentElement.getAttribute("productId");
         //NOTA: OTRA FORMA PODRÍA SER USANDO EL NOMBRE DE LA CLASE, COMO EN LA PRÁCTICA 7
 
         // SE CREA EL OBJETO DE CONEXIÓN ASÍNCRONA AL SERVIDOR
         var client = getXMLHttpRequest();
-        client.open('GET', './backend/product-delete.php?id='+id, true);
+        client.open('GET', './backend/product-delete.php?id=' + id, true);
         client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         client.onreadystatechange = function () {
             // SE VERIFICA SI LA RESPUESTA ESTÁ LISTA Y FUE SATISFACTORIA
@@ -232,21 +232,21 @@ function eliminarProducto() {
 function getXMLHttpRequest() {
     var objetoAjax;
 
-    try{
+    try {
         objetoAjax = new XMLHttpRequest();
-    }catch(err1){
+    } catch (err1) {
         /**
          * NOTA: Las siguientes formas de crear el objeto ya son obsoletas
          *       pero se comparten por motivos historico-académicos.
          */
-        try{
+        try {
             // IE7 y IE8
             objetoAjax = new ActiveXObject("Msxml2.XMLHTTP");
-        }catch(err2){
-            try{
+        } catch (err2) {
+            try {
                 // IE5 y IE6
                 objetoAjax = new ActiveXObject("Microsoft.XMLHTTP");
-            }catch(err3){
+            } catch (err3) {
                 objetoAjax = false;
             }
         }
